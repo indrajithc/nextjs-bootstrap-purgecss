d0a8c1bc019ecfee341c396dd23097181cdfdd62
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-31 16:55:09 +0530

    refactor: bootstrap added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "/node_modules/bootstrap/scss/bootstrap.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
