d54e05402021f031b0a29dc25542a9f0e21bc105
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-31 17:00:02 +0530

    refactor: bootstrap class added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-    <section className="">
+    <section className="container">
       <div className="header">
         <h2>Hello world</h2>
       </div>
